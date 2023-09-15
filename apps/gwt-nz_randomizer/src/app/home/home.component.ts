953c9b56c5b7e5511f3a44a59d1ec5c2f074904a
Vortilion <skleiber@gmail.com>
2023-09-15 14:02:46 +0200

    feat: Added player selection

--- apps/gwt-nz_randomizer/src/app/home/home.component.ts
+++ apps/gwt-nz_randomizer/src/app/home/home.component.ts
@@ -2,6 +2,9 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component, OnInit } from '@angular/core';
 import { ApplicationConfigService } from '../shared/application-config.service';
 import { Tile } from '../models/tile.model';
+import { StorageMap } from '@ngx-pwa/local-storage';
+import { PlayerCountOption } from '../models/player-count-option.model';
+import { MatSelectChange } from '@angular/material/select';
 
 @Component({
   selector: 'app-home',
@@ -12,16 +15,36 @@ export class HomeComponent implements OnInit {
   randomNeutralBuildings: Tile[];
   randomPlayerBuildings: Tile[];
   randomHarborMasters: Tile[];
+  playerCount!: number;
+  playerCountList!: PlayerCountOption[];
   randomDeckbuildingModules: string[];
   isXSmall: boolean;
   isMax768: boolean;
+  isMax1280: boolean;
 
   constructor(
     private applicationConfigService: ApplicationConfigService,
     private responsive: BreakpointObserver,
+    private storage: StorageMap,
   ) {}
 
   ngOnInit(): void {
+    this.playerCount = 2;
+    this.playerCountList = [
+      {
+        label: '2',
+        value: 2,
+      },
+      {
+        label: '3',
+        value: 3,
+      },
+      {
+        label: '4',
+        value: 4,
+      },
+    ];
+
     this.responsive.observe(Breakpoints.XSmall).subscribe((result) => {
       if (result.matches) {
         this.isXSmall = true;
@@ -38,7 +61,36 @@ export class HomeComponent implements OnInit {
       }
     });
 
+    this.responsive.observe('(max-width: 1280px)').subscribe((result) => {
+      if (result.matches) {
+        this.isMax1280 = true;
+      } else {
+        this.isMax1280 = false;
+      }
+    });
+
+    this.storage.get('rar-playerCount').subscribe((playerCount) => {
+      playerCount && typeof playerCount === 'number'
+        ? this.emitPlayerCount(playerCount)
+        : this.storage.set('rar-playerCount', 2).subscribe(() => {});
+    });
+
+    this.applicationConfigService.playerCount.subscribe(
+      (playerCount: number) => {
+        this.playerCount = playerCount;
+      },
+    );
+
     this.randomizeSetup();
+  }
+
+  emitPlayerCount(playerCount: any) {
+    this.applicationConfigService.playerCount.emit(playerCount);
+  }
+
+  onPlayerCountChange(event: MatSelectChange) {
+    this.storage.set('rar-playerCount', event.value).subscribe(() => {});
+    this.emitPlayerCount(event.value);
   }
 
   randomizeSetup() {
--- apps/gwt-nz_randomizer/src/app/page-header/page-header.component.ts
+++ apps/gwt-nz_randomizer/src/app/page-header/page-header.component.ts
@@ -1,12 +1,42 @@
-import { Component, Input } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
   templateUrl: './page-header.component.html',
   styleUrls: ['./page-header.component.scss'],
 })
-export class PageHeaderComponent {
-  @Input() isXSmall!: boolean;
+export class PageHeaderComponent implements OnInit {
   @Input() sidebarHandle: any;
-  @Input() hideSideNavButton: boolean;
+  isXXSmall: boolean;
+  isXSmall: boolean;
+  isMax768: boolean;
+
+  constructor(private responsive: BreakpointObserver) {}
+
+  ngOnInit(): void {
+    this.responsive.observe('(max-width: 480px)').subscribe((result) => {
+      if (result.matches) {
+        this.isXXSmall = true;
+      } else {
+        this.isXXSmall = false;
+      }
+    });
+
+    this.responsive.observe(Breakpoints.XSmall).subscribe((result) => {
+      if (result.matches) {
+        this.isXSmall = true;
+      } else {
+        this.isXSmall = false;
+      }
+    });
+
+    this.responsive.observe('(max-width: 768px)').subscribe((result) => {
+      if (result.matches) {
+        this.isMax768 = true;
+      } else {
+        this.isMax768 = false;
+      }
+    });
+  }
 }
