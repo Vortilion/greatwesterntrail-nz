6d36edd97afe5b7dc3d7477525103adc48bada0c
Vortilion <skleiber@gmail.com>
2023-09-18 11:45:31 +0200

    feat: aut language selection dep on browserlang

--- apps/gwt-nz_randomizer/src/app/language-selector/language-selector.component.ts
+++ apps/gwt-nz_randomizer/src/app/language-selector/language-selector.component.ts
@@ -1,20 +1,37 @@
-import { Component } from '@angular/core';
-import { TranslocoService } from '@ngneat/transloco';
+import { Component, OnInit } from '@angular/core';
+import {
+  AvailableLangs,
+  TranslocoService,
+  getBrowserLang,
+} from '@ngneat/transloco';
 
 @Component({
-    selector: 'app-language-selector',
-    templateUrl: './language-selector.component.html',
-    styleUrls: ['./language-selector.component.scss'],
+  selector: 'app-language-selector',
+  templateUrl: './language-selector.component.html',
+  styleUrls: ['./language-selector.component.scss'],
 })
-export class LanguageSelectorComponent {
-    activeLang: any;
+export class LanguageSelectorComponent implements OnInit {
+  activeLang: any;
 
-    constructor(private translocoService: TranslocoService) {
-        this.activeLang = this.translocoService.getActiveLang();
+  constructor(private translocoService: TranslocoService) {}
+  ngOnInit(): void {
+    const browserLang: string = `${getBrowserLang()}`;
+
+    if (this.translocoService.isLang(browserLang)) {
+      this.activeLang = getBrowserLang();
+      this.translocoService.setActiveLang(this.activeLang);
+    } else {
+      this.activeLang = this.translocoService.getDefaultLang();
     }
 
-    changeLanguage(lang: string): void {
-        this.translocoService.setActiveLang(lang);
-        this.activeLang = this.translocoService.getActiveLang();
-    }
+    console.debug(
+      'this.translocoService.getDefaultLang(): ',
+      this.translocoService.getDefaultLang(),
+    );
+  }
+
+  changeLanguage(lang: string): void {
+    this.translocoService.setActiveLang(lang);
+    this.activeLang = this.translocoService.getActiveLang();
+  }
 }
--- apps/gwt-nz_randomizer/src/app/transloco-root.module.ts
+++ apps/gwt-nz_randomizer/src/app/transloco-root.module.ts
@@ -1,24 +1,26 @@
-import {
-    provideTransloco,
-    TranslocoModule,
-} from '@ngneat/transloco';
+import { provideTransloco, TranslocoModule } from '@ngneat/transloco';
 import { Injectable, isDevMode, NgModule } from '@angular/core';
 
 import { TranslocoHttpLoader } from './transloco-loader';
 
 @NgModule({
-    exports: [TranslocoModule],
-    providers: [
-        provideTransloco({
-            config: {
-                availableLangs: ['de', 'en'],
-                defaultLang: 'de',
-                // Remove this option if your application doesn't support changing language in runtime.
-                reRenderOnLangChange: true,
-                prodMode: !isDevMode(),
-            },
-            loader: TranslocoHttpLoader
-        }),
-    ],
+  exports: [TranslocoModule],
+  providers: [
+    provideTransloco({
+      config: {
+        availableLangs: ['de', 'en'],
+        defaultLang: 'en',
+        fallbackLang: 'en',
+        missingHandler: {
+          // It will use the first language set in the `fallbackLang` property
+          useFallbackTranslation: true,
+        },
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
+  ],
 })
 export class TranslocoRootModule {}
